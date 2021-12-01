4bd362a7b3787bf9f7d3646b1061bb1d5951b5cd
Elisabeth Farias <elisabethf@ciandt.com>
2021-11-30 23:24:20 -0300

    Binding configuration performed in the project

--- app/src/main/java/br/com/dio/currrencyconverter/MainActivity.kt
+++ app/src/main/java/br/com/dio/currrencyconverter/MainActivity.kt
@@ -2,10 +2,14 @@ package br.com.dio.currrencyconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import br.com.dio.currrencyconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
